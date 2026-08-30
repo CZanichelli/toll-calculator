20cea44a3f237d2b39ab0d795480d9f159febae1
Casper Zanichell <casperzanichelli98@gmail.com>
2026-08-31 00:13:19 +0200

    Add vehicle types and toll free vehicles

--- Python/toll_calculator.py
+++ Python/toll_calculator.py
@@ -1,4 +1,5 @@
 from datetime import time
+from enum import Enum
 
 # Fee schedule per the city's requirements. Start inclusive, end exclusive.
 # The schedule differs from the C# original, see README.
@@ -12,8 +13,28 @@ FEES = [
     (time(15, 0), time(15, 30), 13),
     (time(15, 30), time(17, 0), 18),
     (time(17, 0), time(18, 0), 13),
-    (time(18, 0), time(18, 30), 8),   
+    (time(18, 0), time(18, 30), 8), 
 ]
+
+
+class VehicleType(Enum):
+    CAR = "Car"
+    MOTORBIKE = "Motorbike"
+    TRACTOR = "Tractor"
+    EMERGENCY = "Emergency"
+    DIPLOMAT = "Diplomat"
+    FOREIGN = "Foreign"
+    MILITARY = "Military"
+
+
+TOLL_FREE_VEHICLES = frozenset({
+    VehicleType.MOTORBIKE,
+    VehicleType.TRACTOR,
+    VehicleType.EMERGENCY,
+    VehicleType.DIPLOMAT,
+    VehicleType.FOREIGN,
+    VehicleType.MILITARY,
+})
 
 
 def fee_for_time(passage):
